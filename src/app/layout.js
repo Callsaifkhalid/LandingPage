bab258063d6a6251c9a35b317b296372ded76046
Syed Hassan Mustafa <syed.hassanmustafa2070@gmail.com>
2024-01-11 17:19:06 +0500

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-{/* <Head>
+<Head>
   <title>Debt Consolidation Loan | ClearCredit</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta
@@ -62,7 +62,7 @@ export default function RootLayout({ children }) {
       `,
     }}
   />
-</Head> */}
+</Head>
      
       <body className="main">
         <InputProvider>
