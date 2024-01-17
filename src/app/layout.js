d2558b9d8ce0265d9523f96bd68993e91693402a
Syed Hassan Mustafa <syed.hassanmustafa2070@gmail.com>
2024-01-17 11:47:48 +0500

    comit

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,10 +5,17 @@ import Head from "next/head";
 import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "Debt Consolidation Loan | ClearCredit",
-//   description: "Consolidating debt can help you simplify and take control of your finances. Combine balances and make one set monthly payment with a debt consolidation loan.",
-// };
+export const metadata = {
+  title: "Debt Consolidation Loan | ClearCredit",
+  description: "Consolidating debt can help you simplify and take control of your finances. Combine balances and make one set monthly payment with a debt consolidation loan.",
+  metadata:"Debt consolidation, consolidate, consolidation loans, debt consolidation loan, debt, how to get out of debt, unsecured debt, debt, personal loans, personal loan"
+};
+
+const clarityCode = `(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "kj3s2qcy7x");`
 
 export default function RootLayout({ children }) {
   return (
@@ -36,14 +43,6 @@ export default function RootLayout({ children }) {
         />
 
         <meta
-          property="og:title"
-          content="Debt Consolidation Loan | Clear Credit"
-        />
-        <meta
-          property="og:description"
-          content="Consolidating debt can help you simplify and take control of your finances. Combine balances and make one set monthly payment with a debt consolidation loan."
-        />
-        <meta
           property="og:image"
           content="https://www.wellsfargo.com/assets/images/photography/lifestyle/970x462/large_marquee_desktop_woman_at_laptop_reclining_970x462.jpg"
         />
@@ -59,28 +58,9 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-        <script>
-{function (w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != "dataLayer" ? "&l=" + l : "";
-  j.async = true;
-  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-  f.parentNode.insertBefore(j, f);
-}}
-(window,document,'script','dataLayer','GTM-P48NRBMV');
-</script>
-
-<script type="text/javascript">
-    {function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    }}(window, document, "clarity", "script", "kj3s2qcy7x");
-</script>
-
+        <Script id="ms-clarity" strategy="afterInteractive" >
+            {clarityCode}
+        </Script>
 
         {/* <Script>
           {function (w, d, s, l, i) {
