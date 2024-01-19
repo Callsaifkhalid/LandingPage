56c18aaa4ade383d2cd9a3740d0c274ed3720ef0
hassan2770 <syed.hassanmustafa2070@gmail.com>
2024-01-19 13:49:48 +0000

    base url changed

--- src/utils/constantVariables.js
+++ src/utils/constantVariables.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://gateway-clear-debt-credit.dsmeglobal.com/";
+// export const BASE_URL = "https://gateway-clear-debt-credit.dsmeglobal.com/";
+export const BASE_URL = "https://api-gateway-backend.clearcredit.ai/";
